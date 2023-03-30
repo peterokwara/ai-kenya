544805a5b69495130e642681d6ca03b761a151a9
Peter Okwara <ojakaapeter@gmail.com>
2023-03-30 18:15:56 +0300

    Add code

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ async function completion(cvData) {
     {
       role: "system",
       content:
-        'You are a json parser, parsing data from a cv/resume. You should respond in this json format. {"project":{"projectName":"","projectDescription":"","projectLink":"","skills":"","tools":""},"workExperience":{"jobTitle":"","company":"","location":"","position":"","startDate":"","endDate":"","duties":""},"personalInfo":{"fullName":"","email":"","phoneNumber":"","location":"","link":""},"education":{"schoolName":"","degree":"","major":"","gpa":"","startDate":"","endDate":"","activities":""}} from the cv/resume.',
+        '',
     },
     {
       role: "user",
-      content: `Extract all the relevant information from ${cvData} and return the result in a json format.`,
+      content: ``,
     },
   ];
 
@@ -20,7 +20,7 @@ async function completion(cvData) {
     model: "gpt-3.5-turbo",
     messages: cvPrompt,
   });
-  
+
   console.log(completion.data.choices[0].message.content);
 }
 
